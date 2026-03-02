8308b675e00687ce20c1f047a0b3ba1c15d9ba4e
mark daniel avecilla <markdanielavecilla@gmail.com>
2026-03-02 11:12:14 +0800

    updated

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const h1 = document.getElementById("main-title");
-console.dir(h1);
+
+h1.textContent = "Some new title";
+h1.style.color = "white";
+h1.style.backgroundColor = "black";
+
+const li = document.querySelector("li:last-of-type");
+li.textContent = li.textContent + " (Changed!)";
+
+const body = document.body;
+
+const listItemElement = document.getElementsByTagName("li");
+
+for (const listItemEl of listItemElement) {
+	console.dir(listItemEl);
+}
